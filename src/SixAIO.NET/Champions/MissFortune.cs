0df55f4069fcac5db1121f243c44d26bdd34a621
SiXHOiJ <dksovfen@gmail.com>
2021-10-24 01:05:31 +0200

    mf

---
+++ src/SixAIO.NET/Champions/MissFortune.cs
@@ -0,0 +1,73 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class MissFortune : Champion
+    {
+        public MissFortune()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                ShouldCast = (target, spellClass, damage) =>
+                            UseQ &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 60 &&
+                            target != null,
+                TargetSelect = () =>
+                            UnitManager.EnemyChampions
+                            .FirstOrDefault(x => x.Distance <= UnitManager.MyChampion.TrueAttackRange && x.IsAlive && TargetSelector.IsAttackable(x))
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                ShouldCast = (target, spellClass, damage) =>
+                            UseE &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 80 &&
+                            target != null,
+                TargetSelect = () =>
+                            UnitManager.EnemyChampions
+                            .FirstOrDefault(x => x.Distance <= 1000 && x.IsAlive && TargetSelector.IsAttackable(x))
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellQ.ExecuteCastSpell() || SpellE.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void OnCoreLaneClearInput()
+        {
+            if (SpellQ.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void OnCoreHarassInput()
+        {
+            if (SpellQ.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(MissFortune)}"));
+            MenuTab.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            //MenuTab.AddItem(new Switch() { Title = "Use W", IsOn = true });
+            MenuTab.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            //MenuTab.AddItem(new Switch() { Title = "Use R", IsOn = true });
+        }
+    }
+}
