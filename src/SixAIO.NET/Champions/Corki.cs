ee94f361cfc954f714fb808502c1dbf24e3fbc9c
sixhoij <dksovfen@gmail.com>
2021-11-27 11:50:34 +0100

    sona + corki

---
+++ src/SixAIO.NET/Champions/Corki.cs
@@ -0,0 +1,81 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Extensions;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Helpers;
+using SixAIO.Models;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class Corki : Champion
+    {
+        public Corki()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                Range = 825,
+                Speed = 1000,
+                Width = 250,
+                ShouldCast = (target, spellClass, damage) =>
+                            UseQ &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 100 &&
+                            target != null,
+                TargetSelect = () =>
+                {
+                    var ccTarget = UnitManager.EnemyChampions.FirstOrDefault(x => x.Distance <= 825 && x.IsAlive && x.BuffManager.GetBuffList().Any(BuffChecker.IsCrowdControlledOrSlowed));
+                    if (ccTarget != null)
+                    {
+                        return ccTarget;
+                    }
+                    return UnitManager.EnemyChampions.FirstOrDefault(x => x.Distance <= 825 && x.IsAlive);
+                }
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                ShouldCast = (target, spellClass, damage) =>
+                            UseE &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 50 &&
+                            UnitManager.EnemyChampions.FirstOrDefault(x => x.Distance <= UnitManager.MyChampion.TrueAttackRange && x.IsAlive && TargetSelector.IsAttackable(x)) != null,
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                Range = 1300,
+                Speed = 2000,
+                Width = 80,
+                CastTime = 0.2f,
+                ShouldCast = (target, spellClass, damage) =>
+                            UseR &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 20 &&
+                            spellClass.Charges >= 1 &&
+                            target != null,
+                TargetSelect = () => UnitManager.EnemyChampions.FirstOrDefault(x => x.Distance <= 1150 && x.IsAlive &&
+                                                                                    TargetSelector.IsAttackable(x) &&
+                                                                                    !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Physical, false) &&
+                                                                                    x.BuffManager.GetBuffList().Any(BuffChecker.IsCrowdControlledOrSlowed))
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellE.ExecuteCastSpell() || SpellQ.ExecuteCastSpell() || SpellR.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Corki)}"));
+            MenuTab.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            MenuTab.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            MenuTab.AddItem(new Switch() { Title = "Use R", IsOn = true });
+        }
+    }
+}
---
+++ src/SixAIO.NET/Champions/Sona.cs
@@ -0,0 +1,104 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Extensions;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Enums;
+using SixAIO.Helpers;
+using SixAIO.Models;
+using System;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class Sona : Champion
+    {
+        public Sona()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                CastTime = 0f,
+                ShouldCast = (target, spellClass, damage) =>
+                            UseQ &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 70 &&
+                            UnitManager.EnemyChampions.FirstOrDefault(x => x.Distance <= 800 && x.IsAlive && TargetSelector.IsAttackable(x)) != null,
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                CastTime = 0f,
+                ShouldCast = (target, spellClass, damage) =>
+                            UseW &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 40 &&
+                            UnitManager.MyChampion.Mana > WMinMana &&
+                            UnitManager.AllyChampions.FirstOrDefault(x => x.Distance <= 800 && x.IsAlive && (x.Health / x.MaxHealth * 100) < WBuffHealthPercent) != null,
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                CastTime = 0f,
+                ShouldCast = (target, spellClass, damage) =>
+                            UseE &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 65 &&
+                            UnitManager.AllyChampions.FirstOrDefault(x => x.Distance <= 400 && x.IsAlive) != null,
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                Range = 1000,
+                Width = 280,
+                Speed = 2400,
+                ShouldCast = (target, spellClass, damage) =>
+                            UseR &&
+                            spellClass.IsSpellReady &&
+                            UnitManager.MyChampion.Mana > 100 &&
+                            target != null,
+                TargetSelect = () =>
+                            UnitManager.EnemyChampions
+                            .FirstOrDefault(x => x.Distance <= 850 && x.IsAlive &&
+                                                 TargetSelector.IsAttackable(x) && x.BuffManager.GetBuffList().Any(BuffChecker.IsCrowdControlledOrSlowed) &&
+                                                 !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Magical, false))
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellQ.ExecuteCastSpell() || SpellW.ExecuteCastSpell() || SpellE.ExecuteCastSpell() || SpellR.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        private int WBuffHealthPercent
+        {
+            get => MenuTab.GetItem<Counter>("W Buff Health Percent").Value;
+            set => MenuTab.GetItem<Counter>("W Buff Health Percent").Value = value;
+        }
+
+        private int WMinMana
+        {
+            get => MenuTab.GetItem<Counter>("W Min Mana").Value;
+            set => MenuTab.GetItem<Counter>("W Min Mana").Value = value;
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Sona)}"));
+            MenuTab.AddItem(new InfoDisplay() { Title = "---Q Settings---" });
+            MenuTab.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+
+            MenuTab.AddItem(new InfoDisplay() { Title = "---W Settings---" });
+            MenuTab.AddItem(new Switch() { Title = "Use W", IsOn = true });
+            MenuTab.AddItem(new Counter() { Title = "W Min Mana", MinValue = 0, MaxValue = 500, Value = 100, ValueFrequency = 10 });
+            MenuTab.AddItem(new Counter() { Title = "W Buff Health Percent", MinValue = 0, MaxValue = 100, Value = 50, ValueFrequency = 5 });
+
+            MenuTab.AddItem(new InfoDisplay() { Title = "---E Settings---" });
+            MenuTab.AddItem(new Switch() { Title = "Use E", IsOn = true });
+
+            MenuTab.AddItem(new InfoDisplay() { Title = "---R Settings---" });
+            MenuTab.AddItem(new Switch() { Title = "Use R", IsOn = true });
+        }
+    }
+}
