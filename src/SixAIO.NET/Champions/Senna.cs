ebc532ddd46592a8cd57df690f7ebc9e8b09a8b9
sixhoij <dksovfen@gmail.com>
2022-03-04 17:36:36 +0100

    senna

--- src/SixAIO.NET/Champions/Senna.cs
+++ src/SixAIO.NET/Champions/Senna.cs
@@ -5,24 +5,60 @@ using Oasys.SDK;
 using Oasys.SDK.Menu;
 using Oasys.SDK.SpellCasting;
 using SixAIO.Models;
+using System;
 using System.Linq;
 
 namespace SixAIO.Champions
 {
     internal class Senna : Champion
     {
+        public float PassiveStacks()
+        {
+            var passiveStacks = UnitManager.MyChampion.BuffManager.GetActiveBuff("SennaPassiveStacks");
+            return passiveStacks is null ? 0 : passiveStacks.Stacks;
+        }
+
         public Senna()
         {
             SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
             {
                 IsTargetted = () => true,
-                Delay = () => 1f,
+                Delay = () => 0.6f,
                 IsEnabled = () => UseQ,
-                TargetSelect = (mode) => 
-                            UnitManager.EnemyChampions
-                            .FirstOrDefault(x => x.Distance <= UnitManager.MyChampion.TrueAttackRange && x.IsAlive &&
-                                        TargetSelector.IsAttackable(x) &&
-                                        !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Physical, false))
+                Range = () => UnitManager.MyChampion.TrueAttackRange,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode, x => !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Physical, false)).FirstOrDefault()
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                AllowCollision = (target, collisions) => false,
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => WHitChance,
+                Range = () => 1300,
+                Radius = () => 140,
+                Speed = () => 1200,
+                IsEnabled = () => UseW,
+                TargetSelect = (mode) => SpellW.GetTargets(mode).FirstOrDefault()
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                AllowCastOnMap = () => AllowRCastOnMinimap,
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => RHitChance,
+                Range = () => 30000,
+                Radius = () => 320,
+                Speed = () => 20000,
+                Delay = () => 1f,
+                Damage = (target, spellClass) =>
+                            target != null
+                            ? DamageCalculator.GetArmorMod(UnitManager.MyChampion, target) *
+                                        ((125 + spellClass.Level * 125) +
+                                        (UnitManager.MyChampion.UnitStats.BonusAttackDamage) +
+                                        (UnitManager.MyChampion.UnitStats.TotalAbilityPower * 0.7f))
+                            : 0,
+                IsEnabled = () => UseR,
+                MinimumMana = () => RMinMana,
+                ShouldCast = (mode, target, spellClass, damage) => target != null && target.Health < damage,
+                TargetSelect = (mode) => SpellR.GetTargets(mode, x => x.Health <= SpellR.Damage(x, SpellR.SpellClass)).FirstOrDefault()
             };
         }
 
@@ -34,7 +70,31 @@ namespace SixAIO.Champions
 
         internal override void OnCoreMainInput()
         {
-            if (SpellQ.ExecuteCastSpell())
+            if (SpellQ.ExecuteCastSpell() || SpellW.ExecuteCastSpell() || SpellR.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void OnCoreHarassInput()
+        {
+            if (SpellQ.ExecuteCastSpell(Orbwalker.OrbWalkingModeType.Mixed))
+            {
+                return;
+            }
+        }
+
+        internal override void OnCoreLaneClearInput()
+        {
+            if (SpellQ.ExecuteCastSpell(Orbwalker.OrbWalkingModeType.LaneClear))
+            {
+                return;
+            }
+        }
+
+        internal override void OnCoreLastHitInput()
+        {
+            if (SpellQ.ExecuteCastSpell(Orbwalker.OrbWalkingModeType.LastHit))
             {
                 return;
             }
@@ -45,28 +105,23 @@ namespace SixAIO.Champions
             TargetSoulsWithOrbwalker();
         }
 
-        internal override void OnCoreLaneClearInput()
-        {
-            if (SpellQ.ExecuteCastSpell())
-            {
-                return;
-            }
-            //var enemies = UnitManager.EnemyMinions.AsEnumerable<GameObjectBase>().ToList();
-            //enemies.AddRange(UnitManager.EnemyJungleMobs.AsEnumerable<GameObjectBase>());
-            //enemies.AddRange(UnitManager.EnemyChampions.AsEnumerable<GameObjectBase>());
-            //enemies.AddRange(UnitManager.EnemyTowers.AsEnumerable<GameObjectBase>());
-            //var enemy = enemies.Where(x => TargetSelector.IsAttackable(x) && TargetSelector.ShouldAttackMinion(x))
-            //                   .FirstOrDefault(x => x.Distance <= UnitManager.MyChampion.TrueAttackRange + 5 && x.IsAlive);
-            //CastQ(enemy, Oasys.Common.Logic.OrbwalkingMode.LaneClear);
-        }
-
         internal override void InitializeMenu()
         {
             MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Senna)}"));
+            MenuTab.AddItem(new InfoDisplay() { Title = "---Q Settings---" });
             MenuTab.AddItem(new Switch() { Title = "Use Q", IsOn = true });
-            //MenuTab.AddItem(new Switch() { Title = "Use W", IsOn = true });
-            //MenuTab.AddItem(new Switch() { Title = "Use E", IsOn = true });
-            //MenuTab.AddItem(new Switch() { Title = "Use R", IsOn = true });
+
+            MenuTab.AddItem(new InfoDisplay() { Title = "---W Settings---" });
+            MenuTab.AddItem(new Switch() { Title = "Use W", IsOn = true });
+            MenuTab.AddItem(new Counter() { Title = "W Min Mana", MinValue = 0, MaxValue = 500, Value = 80, ValueFrequency = 10 });
+            MenuTab.AddItem(new ModeDisplay() { Title = "W HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            MenuTab.AddItem(new InfoDisplay() { Title = "---R Settings---" });
+            MenuTab.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            MenuTab.AddItem(new Counter() { Title = "R Min Mana", MinValue = 0, MaxValue = 500, Value = 150, ValueFrequency = 10 });
+            MenuTab.AddItem(new Counter() { Title = "R Target Max HP Percent", MinValue = 10, MaxValue = 100, Value = 50, ValueFrequency = 5 });
+            MenuTab.AddItem(new ModeDisplay() { Title = "R HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+            MenuTab.AddItem(new Switch() { Title = "Allow R cast on minimap", IsOn = true });
         }
     }
 }
