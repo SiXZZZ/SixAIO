ff921fe9cea352c726e4cd18e832ad8aaa408c4b
sixhoij <dksovfen@gmail.com>
2022-04-08 23:06:47 +0200

    morgana

--- src/SixAIO.NET/Champions/Morgana.cs
+++ src/SixAIO.NET/Champions/Morgana.cs
@@ -26,20 +26,61 @@ namespace SixAIO.Champions
                 IsEnabled = () => UseQ,
                 TargetSelect = (mode) => SpellQ.GetTargets(mode, x => !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Magical, false)).FirstOrDefault(),
             };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => WHitChance,
+                Range = () => 900,
+                Speed = () => 5000,
+                Radius = () => 260,
+                IsEnabled = () => UseW,
+                TargetSelect = (mode) => SpellW.GetTargets(mode).FirstOrDefault()
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                IsEnabled = () => UseR,
+                MinimumMana = () => RMinMana,
+                ShouldCast = (mode, target, spellClass, damage) => UnitManager.EnemyChampions.Count(x => TargetSelector.IsAttackable(x) && x.Distance < REnemiesCloserThan) > RIfMoreThanEnemiesNear,
+            };
         }
 
         internal override void OnCoreMainInput()
         {
-            SpellQ.ExecuteCastSpell();
+            if (SpellQ.ExecuteCastSpell() || SpellW.ExecuteCastSpell() || SpellR.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        private int RIfMoreThanEnemiesNear
+        {
+            get => RSettings.GetItem<Counter>("R If More Than Enemies Near").Value;
+            set => RSettings.GetItem<Counter>("R If More Than Enemies Near").Value = value;
+        }
+
+        private int REnemiesCloserThan
+        {
+            get => RSettings.GetItem<Counter>("R Enemies Closer Than").Value;
+            set => RSettings.GetItem<Counter>("R Enemies Closer Than").Value = value;
         }
 
         internal override void InitializeMenu()
         {
             MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Morgana)}"));
             MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
+            MenuTab.AddGroup(new Group("R Settings"));
+
             QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
             QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
 
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+            WSettings.AddItem(new ModeDisplay() { Title = "W HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            RSettings.AddItem(new Counter() { Title = "R Min Mana", MinValue = 0, MaxValue = 500, Value = 150, ValueFrequency = 10 });
+            RSettings.AddItem(new Counter() { Title = "R If More Than Enemies Near", MinValue = 0, MaxValue = 5, Value = 1, ValueFrequency = 1 });
+            RSettings.AddItem(new Counter() { Title = "R Enemies Closer Than", MinValue = 50, MaxValue = 575, Value = 575, ValueFrequency = 25 });
         }
     }
 }
