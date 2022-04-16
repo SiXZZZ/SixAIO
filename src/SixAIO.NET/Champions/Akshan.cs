4fbd3cbc8a7f573b3fa1397d5d3cf2b5fd39a8df
sixhoij <dksovfen@gmail.com>
2022-04-16 16:37:31 +0200

    akshan qr

---
+++ src/SixAIO.NET/Champions/Akshan.cs
@@ -0,0 +1,65 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Enums;
+using SixAIO.Models;
+using System;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class Akshan : Champion
+    {
+        public Akshan()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => QHitChance,
+                Range = () => 850,
+                Radius = () => 120,
+                Speed = () => 1500,
+                IsEnabled = () => UseQ,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault(),
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                AllowCastOnMap = () => AllowRCastOnMinimap,
+                IsTargetted = () => true,
+                Delay = () => 3f,
+                Range = () => 2500f,
+                IsEnabled = () => UseR,
+                TargetSelect = (mode) => SpellR.GetTargets(mode, x => x.HealthPercent <= RHealthPercent).FirstOrDefault()
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellQ.ExecuteCastSpell() || SpellR.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        private int RHealthPercent
+        {
+            get => RSettings.GetItem<Counter>("Below health percent").Value;
+            set => RSettings.GetItem<Counter>("Below health percent").Value = value;
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Akshan)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("R Settings"));
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            RSettings.AddItem(new Counter() { Title = "Below health percent", MinValue = 0, MaxValue = 100, Value = 40, ValueFrequency = 5 });
+        }
+    }
+}
