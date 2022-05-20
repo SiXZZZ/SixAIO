dfb22d96d7b20c95e88290771a30174c3d934636
sixhoij <dksovfen@gmail.com>
2022-05-20 20:07:16 +0200

    jhin

---
+++ src/SixAIO.NET/Champions/Jhin.cs
@@ -0,0 +1,96 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SixAIO.Champions
+{
+    internal class Jhin : Champion
+    {
+        public Jhin()
+        {
+            Oasys.SDK.InputProviders.KeyboardProvider.OnKeyPress += KeyboardProvider_OnKeyPress;
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                AllowCollision = (target, collisions) => !collisions.Any(),
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => WHitChance,
+                Range = () => 2500,
+                Radius = () => 90,
+                Speed = () => 2500,
+                Delay = () => 0.75f,
+                IsEnabled = () => UseW,
+                TargetSelect = (mode) => SpellW.GetTargets(mode, x => x.BuffManager.ActiveBuffs.Any(buff => buff.Name == "jhinespotteddebuff" && buff.Stacks >= 1)).FirstOrDefault()
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                IsChannel = () => true,
+                AllowCastOnMap = () => AllowRCastOnMinimap,
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => RHitChance,
+                MinimumCharges = () => 1,
+                Range = () => 3500,
+                Radius = () => 160,
+                Speed = () => 5000,
+                IsEnabled = () => UseR && SpellR.SpellClass.SpellData.SpellName == "JhinRShot",
+                IsSpellReady = (spellClass, minMana, minCharges) => spellClass.Charges > minCharges || UnitManager.MyChampion.Mana > minMana,
+                TargetSelect = (mode) => SpellR.GetTargets(mode).OrderBy(x => x.DistanceTo(GameEngine.WorldMousePosition)).FirstOrDefault()
+            };
+            SpellRSemiAuto = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                IsChannel = () => true,
+                AllowCastOnMap = () => AllowRCastOnMinimap,
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => SemiAutoRHitChance,
+                MinimumCharges = () => 1,
+                Range = () => 3500,
+                Radius = () => 160,
+                Speed = () => 5000,
+                IsEnabled = () => UseSemiAutoR && SpellRSemiAuto.SpellClass.SpellData.SpellName == "JhinRShot",
+                IsSpellReady = (spellClass, minMana, minCharges) => spellClass.Charges > minCharges || UnitManager.MyChampion.Mana > minMana,
+                TargetSelect = (mode) => SpellRSemiAuto.GetTargets(mode).OrderBy(x => x.DistanceTo(GameEngine.WorldMousePosition)).FirstOrDefault()
+            };
+        }
+
+        private void KeyboardProvider_OnKeyPress(Keys keyBeingPressed, Oasys.Common.Tools.Devices.Keyboard.KeyPressState pressState)
+        {
+            if (keyBeingPressed == SemiAutoRKey && pressState == Oasys.Common.Tools.Devices.Keyboard.KeyPressState.Down)
+            {
+                SpellRSemiAuto.ExecuteCastSpell();
+            }
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellW.ExecuteCastSpell() || SpellR.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Jhin)}"));
+            MenuTab.AddGroup(new Group("W Settings"));
+            MenuTab.AddGroup(new Group("R Settings"));
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+            WSettings.AddItem(new ModeDisplay() { Title = "W HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            RSettings.AddItem(new Switch() { Title = "Allow R cast on minimap", IsOn = true });
+            RSettings.AddItem(new ModeDisplay() { Title = "R HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            RSettings.AddItem(new Switch() { Title = "Use Semi Auto R", IsOn = true });
+            RSettings.AddItem(new KeyBinding() { Title = "Semi Auto R Key", SelectedKey = Keys.T });
+            RSettings.AddItem(new ModeDisplay() { Title = "Semi Auto R HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+        }
+    }
+}
