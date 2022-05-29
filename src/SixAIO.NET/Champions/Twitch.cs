6352b1394b090202ee7aa78956918e0f4967037b
sixhoij <dksovfen@gmail.com>
2022-05-29 21:04:11 +0200

    twitch combo stuff

--- src/SixAIO.NET/Champions/Twitch.cs
+++ src/SixAIO.NET/Champions/Twitch.cs
@@ -1,4 +1,5 @@
-﻿using Oasys.Common.Enums.GameEnums;
+﻿using Oasys.Common;
+using Oasys.Common.Enums.GameEnums;
 using Oasys.Common.GameObject;
 using Oasys.Common.GameObject.ObjectClass;
 using Oasys.Common.Menu;
@@ -8,6 +9,7 @@ using Oasys.SDK.Menu;
 using Oasys.SDK.SpellCasting;
 using Oasys.SDK.Tools;
 using SixAIO.Models;
+using System;
 using System.Linq;
 
 namespace SixAIO.Champions
@@ -16,11 +18,40 @@ namespace SixAIO.Champions
     {
         public Twitch()
         {
+            Orbwalker.OnOrbwalkerAfterBasicAttack += Orbwalker_OnOrbwalkerAfterBasicAttack;
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                IsEnabled = () => UseQ,
+                ShouldCast = (mode, target, spellClass, damage) =>
+                            SpellQ.SpellClass.CooldownExpire < _lastQUse &&
+                            !UnitManager.MyChampion.BuffManager.ActiveBuffs.Any(x => x.Name == "TwitchHideInShadows" && x.Stacks >= 1) &&
+                            UnitManager.EnemyChampions.Count(x => TargetSelector.IsAttackable(x) && x.Distance < QEnemiesCloserThan) > QIfMoreThanEnemiesNear,
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => WHitChance,
+                Range = () => 950,
+                Speed = () => 1400,
+                Radius = () => 260,
+                IsEnabled = () => UseW,
+                TargetSelect = (mode) => SpellW.GetTargets(mode).FirstOrDefault()
+            };
             SpellE = new Spell(CastSlot.E, SpellSlot.E)
             {
                 IsEnabled = () => UseE,
                 ShouldCast = (mode, target, spellClass, damage) => ShouldCastE(mode),
             };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                IsEnabled = () => UseR,
+                ShouldCast = (mode, target, spellClass, damage) => UnitManager.EnemyChampions.Count(x => TargetSelector.IsAttackable(x) && x.Distance < REnemiesCloserThan) > RIfMoreThanEnemiesNear,
+            };
+        }
+
+        private void Orbwalker_OnOrbwalkerAfterBasicAttack(float gameTime, GameObjectBase target)
+        {
+            SpellW.ExecuteCastSpell();
         }
 
         internal bool ShouldCastE(Orbwalker.OrbWalkingModeType mode)
@@ -63,12 +94,20 @@ namespace SixAIO.Champions
                    (UnitManager.MyChampion.Inventory.HasItem(ItemID.The_Collector) && (target.Health - dmg) / target.MaxHealth * 100f < 5f);
         }
 
+        private float _lastQUse;
+        internal override void OnCoreMainTick()
+        {
+            if (!SpellQ.SpellClass.IsSpellReady)
+            {
+                _lastQUse = SpellQ.SpellClass.CooldownExpire;
+            }
+        }
+
         internal override void OnCoreMainInput()
         {
-            if (SpellE.ExecuteCastSpell())
-            {
-                return;
-            }
+            SpellE.ExecuteCastSpell();
+            SpellQ.ExecuteCastSpell();
+            SpellR.ExecuteCastSpell();
         }
 
         internal override void OnCoreLaneClearInput()
@@ -98,6 +137,18 @@ namespace SixAIO.Champions
             return (float)(((physicalDamage - enemy.PhysicalShield) + (magicDamage - enemy.MagicalShield)) - enemy.NeutralShield);
         }
 
+        private int QIfMoreThanEnemiesNear
+        {
+            get => QSettings.GetItem<Counter>("Q If More Than Enemies Near").Value;
+            set => QSettings.GetItem<Counter>("Q If More Than Enemies Near").Value = value;
+        }
+
+        private int QEnemiesCloserThan
+        {
+            get => QSettings.GetItem<Counter>("Q Enemies Closer Than").Value;
+            set => QSettings.GetItem<Counter>("Q Enemies Closer Than").Value = value;
+        }
+
         private bool EWhenCanKill
         {
             get => ESettings.GetItem<Switch>("E when can kill").IsOn;
@@ -116,16 +167,42 @@ namespace SixAIO.Champions
             set => ESettings.GetItem<Counter>("Minimum stacks").Value = value;
         }
 
+        private int RIfMoreThanEnemiesNear
+        {
+            get => RSettings.GetItem<Counter>("R If More Than Enemies Near").Value;
+            set => RSettings.GetItem<Counter>("R If More Than Enemies Near").Value = value;
+        }
+
+        private int REnemiesCloserThan
+        {
+            get => RSettings.GetItem<Counter>("R Enemies Closer Than").Value;
+            set => RSettings.GetItem<Counter>("R Enemies Closer Than").Value = value;
+        }
+
         internal override void InitializeMenu()
         {
             MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Twitch)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
             MenuTab.AddGroup(new Group("E Settings"));
+            MenuTab.AddGroup(new Group("R Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new Counter() { Title = "Q If More Than Enemies Near", MinValue = 0, MaxValue = 5, Value = 0, ValueFrequency = 1 });
+            QSettings.AddItem(new Counter() { Title = "Q Enemies Closer Than", MinValue = 50, MaxValue = 1500, Value = 800, ValueFrequency = 25 });
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+            WSettings.AddItem(new ModeDisplay() { Title = "W HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
 
             ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
             ESettings.AddItem(new Switch() { Title = "Use E Laneclear", IsOn = true });
             ESettings.AddItem(new Counter() { Title = "Targets with stacks", MinValue = 1, MaxValue = 5, Value = 1, ValueFrequency = 1 });
             ESettings.AddItem(new Counter() { Title = "Minimum stacks", MinValue = 1, MaxValue = 6, Value = 6, ValueFrequency = 1 });
             ESettings.AddItem(new Switch() { Title = "E when can kill", IsOn = true });
+
+            RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            RSettings.AddItem(new Counter() { Title = "R If More Than Enemies Near", MinValue = 0, MaxValue = 5, Value = 2, ValueFrequency = 1 });
+            RSettings.AddItem(new Counter() { Title = "R Enemies Closer Than", MinValue = 50, MaxValue = 1500, Value = 1100, ValueFrequency = 50 });
         }
     }
 }
