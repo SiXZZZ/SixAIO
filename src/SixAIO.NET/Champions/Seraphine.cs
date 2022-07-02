77e76f42f7b37ac5dccdae67d825a89526de0a2b
sixhoij <dksovfen@gmail.com>
2022-07-02 11:35:47 +0200

    Seraphine

---
+++ src/SixAIO.NET/Champions/Seraphine.cs
@@ -0,0 +1,84 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Extensions;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class Seraphine : Champion
+    {
+        public Seraphine()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => QHitChance,
+                Range = () => 900,
+                Radius = () => 350,
+                Speed = () => 1200,
+                IsEnabled = () => UseQ,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault()
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                ShouldCast = (mode, target, spellClass, damage) => UnitManager.EnemyChampions.Any(x => x.Distance < 750 && TargetSelector.IsAttackable(x))
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => EHitChance,
+                Range = () => 1300,
+                Radius = () => 140,
+                Speed = () => 1200,
+                IsEnabled = () => UseE,
+                TargetSelect = (mode) => SpellE.GetTargets(mode).FirstOrDefault()
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => RHitChance,
+                Delay = () => 0.5f,
+                Range = () => 1200,
+                Radius = () => 320,
+                Speed = () => 1600,
+                IsEnabled = () => UseR,
+                TargetSelect = (mode) => SpellR.GetTargets(mode, x => !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Magical, false)).FirstOrDefault()
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellE.ExecuteCastSpell() || SpellQ.ExecuteCastSpell() || SpellW.ExecuteCastSpell() || SpellR.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Seraphine)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+            MenuTab.AddGroup(new Group("R Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            ESettings.AddItem(new ModeDisplay() { Title = "E HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "VeryHigh" });
+
+            RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            RSettings.AddItem(new ModeDisplay() { Title = "R HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "Immobile" });
+        }
+    }
+}
