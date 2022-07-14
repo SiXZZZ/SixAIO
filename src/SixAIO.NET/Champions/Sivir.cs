1fb34de9f8a64c2c6d9026b9f9e5e373846d1f3e
sixhoij <dksovfen@gmail.com>
2022-07-14 17:28:34 +0200

    sivir

---
+++ src/SixAIO.NET/Champions/Sivir.cs
@@ -0,0 +1,66 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class Sivir : Champion
+    {
+        public Sivir()
+        {
+            Spell.OnSpellCast += Spell_OnSpellCast;
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                AllowCollision = (target, collisions) => !collisions.Any(),
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => QHitChance,
+                Range = () => 1250,
+                Radius = () => 180,
+                Speed = () => 1400,
+                IsEnabled = () => UseQ,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault()
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                IsSpellReady = (spellClass, minimumMana, minimumCharges) => spellClass.IsSpellReady && UnitManager.MyChampion.Mana > 80,
+                ShouldCast = (mode, target, spellClass, damage) => TargetSelector.IsAttackable(Orbwalker.TargetHero) && TargetSelector.IsInRange(Orbwalker.TargetHero),
+            };
+        }
+
+        private void Spell_OnSpellCast(SDKSpell arg1, Oasys.Common.GameObject.GameObjectBase arg2)
+        {
+            if (arg1.CastSlot == CastSlot.W)
+            {
+                Orbwalker.AttackReset();
+            }
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellW.ExecuteCastSpell() || SpellQ.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Sivir)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+
+        }
+    }
+}
