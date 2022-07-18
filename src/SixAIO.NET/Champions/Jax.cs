fe1701a13b688384c5400c5f732aea486b177ee8
sixhoij <dksovfen@gmail.com>
2022-07-18 13:45:28 +0200

    Jax

---
+++ src/SixAIO.NET/Champions/Jax.cs
@@ -0,0 +1,84 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.GameObject;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class Jax : Champion
+    {
+        public Jax()
+        {
+            Spell.OnSpellCast += Spell_OnSpellCast;
+            Orbwalker.OnOrbwalkerAfterBasicAttack += Orbwalker_OnOrbwalkerAfterBasicAttack;
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                IsTargetted = () => true,
+                Range = () => 700,
+                IsEnabled = () => UseQ &&
+                            !TargetSelector.IsAttackable(Orbwalker.TargetHero) &&
+                            !TargetSelector.IsInRange(Orbwalker.TargetHero),
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault()
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                ShouldCast = (mode, target, spellClass, damage) =>
+                            !Orbwalker.CanBasicAttack &&
+                            TargetSelector.IsAttackable(Orbwalker.TargetHero) &&
+                            TargetSelector.IsInRange(Orbwalker.TargetHero),
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                IsEnabled = () => UseE,
+                ShouldCast = (mode, target, spellClass, damage) => UnitManager.EnemyChampions.Any(x => x.Distance <= 300 && TargetSelector.IsAttackable(x))
+            };
+        }
+
+        private void Spell_OnSpellCast(SDKSpell spell, GameObjectBase target)
+        {
+            if (spell.CastSlot == CastSlot.W)
+            {
+                Orbwalker.AttackReset();
+            }
+        }
+
+        private void Orbwalker_OnOrbwalkerAfterBasicAttack(float gameTime, GameObjectBase target)
+        {
+            if (target != null)
+            {
+                SpellW.ExecuteCastSpell();
+            }
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellQ.ExecuteCastSpell() || SpellE.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Jax)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+
+
+        }
+    }
+}
