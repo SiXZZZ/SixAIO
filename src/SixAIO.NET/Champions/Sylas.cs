f7de46c181a40ea8dc406139a39174281cf57e19
sixhoij <dksovfen@gmail.com>
2022-07-31 14:01:14 +0200

    sylas

---
+++ src/SixAIO.NET/Champions/Sylas.cs
@@ -0,0 +1,76 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.GameObject;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using Oasys.SDK.Tools;
+using SixAIO.Enums;
+using SixAIO.Models;
+using System;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class Sylas : Champion
+    {
+        private bool IsEActive => UnitManager.MyChampion.GetSpellBook().GetSpellClass(SpellSlot.E).SpellData.SpellName.Equals("SylasE2", StringComparison.OrdinalIgnoreCase);
+        //SylasPassiveAttack
+        public Sylas()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => QHitChance,
+                Range = () => 775,
+                Radius = () => 120,
+                Speed = () => 2000,
+                IsEnabled = () => UseQ,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault()
+            };
+            SpellW = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => QHitChance,
+                Range = () => 775,
+                Speed = () => 2000,
+                Radius = () => 180,
+                IsEnabled = () => UseQ,
+                TargetSelect = (mode) => SpellW.GetTargets(mode).FirstOrDefault()
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                AllowCollision = (target, collisions) => !IsEActive,
+                Speed = () => IsEActive ? 1600 : 0,
+                Radius = () => IsEActive ? 100 : 0,
+                Range = () => IsEActive ? 800 : 0,
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                IsTargetted = () => !IsEActive,
+                IsEnabled = () => UseE,
+                ShouldCast = (mode, target, spellClass, damage) => target is not null && TargetSelector.IsAttackable(target) && !TargetSelector.IsInRange(target),
+                TargetSelect = (mode) => UnitManager.EnemyChampions.FirstOrDefault(x => x.Distance <= x.TrueAttackRange + 800 && x.IsAlive && TargetSelector.IsAttackable(x))
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (SpellQ.ExecuteCastSpell() || SpellW.ExecuteCastSpell() || SpellE.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Sylas)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+        }
+    }
+}
