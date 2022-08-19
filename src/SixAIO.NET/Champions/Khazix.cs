a84794f60fe9294e6dd60a692a2a83f308c18c3f
sixhoij <dksovfen@gmail.com>
2022-08-19 17:13:59 +0200

    khazix

--- src/SixAIO.NET/Champions/Khazix.cs
+++ src/SixAIO.NET/Champions/Khazix.cs
@@ -1,28 +1,31 @@
-﻿using Oasys.Common.Enums.GameEnums;
+﻿using Oasys.Common;
+using Oasys.Common.Enums.GameEnums;
 using Oasys.Common.GameObject;
+using Oasys.Common.GameObject.Clients;
 using Oasys.Common.GameObject.ObjectClass;
 using Oasys.Common.Menu;
 using Oasys.Common.Menu.ItemComponents;
 using Oasys.SDK;
 using Oasys.SDK.Menu;
+using Oasys.SDK.Rendering;
 using Oasys.SDK.SpellCasting;
 using Oasys.SDK.Tools;
 using SixAIO.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SixAIO.Champions
 {
     internal class Khazix : Champion
     {
-        /*
-        [12.36.37 pm - SixAIO]: Q : BlindMonkQOne
-        [12.36.37 pm - SixAIO]: W : BlindMonkWOne
-        [12.36.37 pm - SixAIO]: E : BlindMonkEOne
-        [12.36.37 pm - SixAIO]: R : BlindMonkRKick
-         */
+        private static List<GameObjectBase> _isolations = new();
+        private static IEnumerable<GameObjectBase> Isolations() => _isolations.Where(x => x.Distance <= 2000).Where(IsIsolation);
 
-        private bool IsFirstCast(string spellName) => !spellName.Contains("two", StringComparison.OrdinalIgnoreCase);
+        private static bool IsIsolation(GameObjectBase obj)
+        {
+            return obj.Name.Contains("Khazix", StringComparison.OrdinalIgnoreCase) && obj.Name.Contains("SingleEnemy_Indicator", StringComparison.OrdinalIgnoreCase);
+        }
 
         public Khazix()
         {
@@ -44,14 +47,91 @@ namespace SixAIO.Champions
                 IsEnabled = () => UseW && UnitManager.MyChampion.HealthPercent <= WIfHealthPercentBelow,
                 TargetSelect = (mode) => SpellW.GetTargets(mode).FirstOrDefault()
             };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => EHitChance,
+                Range = () => 900,
+                Speed = () => 1500,
+                Radius = () => 260,
+                IsEnabled = () => UseE,
+                TargetSelect = (mode) => OnlyEIfCanKill
+                                    ? SpellE.GetTargets(mode, x => x.Distance >= EMinimumJumpRange).FirstOrDefault(x => DamageCalculator.GetTargetHealthAfterBasicAttack(UnitManager.MyChampion, x) <= ComboDamage(x))
+                                    : SpellE.GetTargets(mode, x => x.Distance >= EMinimumJumpRange).FirstOrDefault()
+            };
+        }
+
+        private float ComboDamage(GameObjectBase target)
+        {
+            var dmg = 0f;
+            if (SpellQ.SpellClass.IsSpellReady)
+            {
+                dmg += QDamage(target);
+                //Logger.Log($"Combo damage Q: {dmg}");
+            }
+            if (SpellW.SpellClass.IsSpellReady)
+            {
+                dmg += WDamage(target);
+                //Logger.Log($"Combo damage W: {dmg}");
+            }
+            if (SpellE.SpellClass.IsSpellReady)
+            {
+                dmg += EDamage(target);
+                //Logger.Log($"Combo damage E: {dmg}");
+            }
+            //Logger.Log($"Combo damage: {dmg}");
+            return dmg;
+        }
+
+        private float QDamage(GameObjectBase target)
+        {
+            var baseDmg = 45f + SpellQ.SpellClass.Level * 25f;
+            var scaleDmg = 1.15f * UnitManager.MyChampion.UnitStats.BonusAttackDamage;
+            var dmg = baseDmg + scaleDmg;
+            if (IsIsolated(target))
+            {
+                dmg *= 2.1f;
+            }
+            return DamageCalculator.CalculateActualDamage(UnitManager.MyChampion, target, dmg);
+        }
+
+        private float WDamage(GameObjectBase target)
+        {
+            var baseDmg = 55f + SpellW.SpellClass.Level * 30f;
+            var scaleDmg = UnitManager.MyChampion.UnitStats.BonusAttackDamage;
+            return DamageCalculator.CalculateActualDamage(UnitManager.MyChampion, target, baseDmg + scaleDmg);
+        }
+
+        private float EDamage(GameObjectBase target)
+        {
+            var baseDmg = 30f + SpellE.SpellClass.Level * 35f;
+            var scaleDmg = 0.2f * UnitManager.MyChampion.UnitStats.BonusAttackDamage;
+            return DamageCalculator.CalculateActualDamage(UnitManager.MyChampion, target, baseDmg + scaleDmg);
+        }
+
+        private bool IsIsolated(GameObjectBase target)
+        {
+            return Isolations().Any(x => x.DistanceTo(target.Position) <= 50);
         }
 
         internal override void OnCoreMainInput()
         {
-            if (SpellW.ExecuteCastSpell() || SpellQ.ExecuteCastSpell() /*|| SpellE.ExecuteCastSpell()*/)
+            SpellW.ExecuteCastSpell();
+            SpellQ.ExecuteCastSpell();
+            SpellE.ExecuteCastSpell();
+        }
+
+        internal override void OnCreateObject(AIBaseClient obj)
+        {
+            if (IsIsolation(obj))
             {
-                return;
+                _isolations.Add(obj);
             }
+        }
+
+        internal override void OnDeleteObject(AIBaseClient obj)
+        {
+            _isolations.Remove(obj);
         }
 
         private int WMaximumRange
@@ -66,13 +146,24 @@ namespace SixAIO.Champions
             set => WSettings.GetItem<Counter>("W If Health Percent Below").Value = value;
         }
 
+        internal bool OnlyEIfCanKill
+        {
+            get => ESettings.GetItem<Switch>("Only E If Can Kill").IsOn;
+            set => ESettings.GetItem<Switch>("Only E If Can Kill").IsOn = value;
+        }
+
+        internal int EMinimumJumpRange
+        {
+            get => ESettings.GetItem<Counter>("E Minimum Jump Range").Value;
+            set => ESettings.GetItem<Counter>("E Minimum Jump Range").Value = value;
+        }
+
         internal override void InitializeMenu()
         {
             MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Khazix)}"));
             MenuTab.AddGroup(new Group("Q Settings"));
             MenuTab.AddGroup(new Group("W Settings"));
             MenuTab.AddGroup(new Group("E Settings"));
-            MenuTab.AddGroup(new Group("R Settings"));
 
             QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
 
@@ -81,9 +172,11 @@ namespace SixAIO.Champions
             WSettings.AddItem(new Counter() { Title = "W Maximum Range", MinValue = 0, MaxValue = 1000, Value = 950, ValueFrequency = 50 });
             WSettings.AddItem(new Counter() { Title = "W If Health Percent Below", MinValue = 0, MaxValue = 100, Value = 80, ValueFrequency = 5 });
 
-            //ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            ESettings.AddItem(new ModeDisplay() { Title = "E HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "VeryHigh" });
+            ESettings.AddItem(new Switch() { Title = "Only E If Can Kill", IsOn = true });
+            ESettings.AddItem(new Counter() { Title = "E Minimum Jump Range", MinValue = 0, MaxValue = 900, Value = 500, ValueFrequency = 25 });
 
-            //RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
         }
     }
 }
---
+++ src/SixAIO.NET/Champions/Rengar.cs
@@ -0,0 +1,126 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Extensions;
+using Oasys.Common.GameObject;
+using Oasys.Common.GameObject.ObjectClass;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using Oasys.SDK.Tools;
+using SixAIO.Models;
+using System;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal class Rengar : Champion
+    {
+        private bool IsEmpowered => UnitManager.MyChampion.Mana == 4;
+
+        public Rengar()
+        {
+            Orbwalker.OnOrbwalkerAfterBasicAttack += Orbwalker_OnOrbwalkerAfterBasicAttack;
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                IsEnabled = () => UseQ && (!IsEmpowered || CanUseEmpoweredQ),
+                ShouldCast = (mode, target, spellClass, damage) => TargetSelector.IsAttackable(Orbwalker.TargetHero) && TargetSelector.IsInRange(Orbwalker.TargetHero),
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW && (!IsEmpowered || CanUseEmpoweredW),
+                ShouldCast = (mode, target, spellClass, damage) => UnitManager.MyChampion.HealthPercent <= WIfHealthPercentBelow || UnitManager.EnemyChampions.Any(x => TargetSelector.IsAttackable(x) && x.Distance <= 450 && x.IsAlive),
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                AllowCollision = (target, collisions) => !collisions.Any(),
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => EHitChance,
+                Range = () => EMaximumRange,
+                Radius = () => 140,
+                Speed = () => 1500,
+                IsEnabled = () => UseE && (!IsEmpowered || CanUseEmpoweredE) && (!OnlyEOutOfAARange || UnitManager.EnemyChampions.All(x => x.Distance >= 200)),
+                TargetSelect = (mode) => SpellE.GetTargets(mode).FirstOrDefault()
+            };
+        }
+
+        private void Orbwalker_OnOrbwalkerAfterBasicAttack(float gameTime, GameObjectBase target)
+        {
+            SpellQ.ExecuteCastSpell();
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            if (UnitManager.MyChampion.AttackRange >= 350)
+            {
+                Orbwalker.SelectedTarget = UnitManager.EnemyChampions
+                    .Where(x => x.Position.Distance(GameEngine.WorldMousePosition) <= 400)
+                    .OrderBy(x => x.Position.Distance(GameEngine.WorldMousePosition))
+                    .FirstOrDefault();
+            }
+
+            if (SpellW.ExecuteCastSpell() || SpellE.ExecuteCastSpell())
+            {
+                return;
+            }
+        }
+
+        internal bool CanUseEmpoweredQ
+        {
+            get => QSettings.GetItem<Switch>("Can Use Empowered Q").IsOn;
+            set => QSettings.GetItem<Switch>("Can Use Empowered Q").IsOn = value;
+        }
+
+        internal bool CanUseEmpoweredW
+        {
+            get => WSettings.GetItem<Switch>("Can Use Empowered W").IsOn;
+            set => WSettings.GetItem<Switch>("Can Use Empowered W").IsOn = value;
+        }
+
+        private int WIfHealthPercentBelow
+        {
+            get => WSettings.GetItem<Counter>("W If Health Percent Below").Value;
+            set => WSettings.GetItem<Counter>("W If Health Percent Below").Value = value;
+        }
+
+        internal bool CanUseEmpoweredE
+        {
+            get => ESettings.GetItem<Switch>("Can Use Empowered E").IsOn;
+            set => ESettings.GetItem<Switch>("Can Use Empowered E").IsOn = value;
+        }
+
+        internal bool OnlyEOutOfAARange
+        {
+            get => ESettings.GetItem<Switch>("Only E Out Of AA Range").IsOn;
+            set => ESettings.GetItem<Switch>("Only E Out Of AA Range").IsOn = value;
+        }
+
+        private int EMaximumRange
+        {
+            get => ESettings.GetItem<Counter>("E Maximum Range").Value;
+            set => ESettings.GetItem<Counter>("E Maximum Range").Value = value;
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Rengar)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new Switch() { Title = "Can Use Empowered Q", IsOn = true });
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+            WSettings.AddItem(new Switch() { Title = "Can Use Empowered W", IsOn = true });
+            WSettings.AddItem(new Counter() { Title = "W If Health Percent Below", MinValue = 0, MaxValue = 100, Value = 50, ValueFrequency = 5 });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            ESettings.AddItem(new Switch() { Title = "Can Use Empowered E", IsOn = true });
+            ESettings.AddItem(new Switch() { Title = "Only E Out Of AA Range", IsOn = true });
+            ESettings.AddItem(new ModeDisplay() { Title = "E HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "VeryHigh" });
+            ESettings.AddItem(new Counter() { Title = "E Maximum Range", MinValue = 0, MaxValue = 1000, Value = 950, ValueFrequency = 50 });
+
+        }
+    }
+}
