91c3ef4c378554b37cde8c4834711e05437a0b43
sixhoij <dksovfen@gmail.com>
2022-08-23 13:52:29 +0200

    tf cards

--- src/SixAIO.NET/Champions/TwistedFate.cs
+++ src/SixAIO.NET/Champions/TwistedFate.cs
@@ -7,11 +7,17 @@ using Oasys.SDK.SpellCasting;
 using SixAIO.Models;
 using System;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace SixAIO.Champions
 {
     internal class TwistedFate : Champion
     {
+        internal Spell YellowCard;
+        internal Spell RedCard;
+        internal Spell BlueCard;
+        internal Spell ManualCard;
+
         private enum Card
         {
             None,
@@ -30,6 +36,7 @@ namespace SixAIO.Champions
 
         public TwistedFate()
         {
+            Oasys.SDK.InputProviders.KeyboardProvider.OnKeyPress += KeyboardProvider_OnKeyPress;
             SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
             {
                 PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
@@ -52,6 +59,87 @@ namespace SixAIO.Champions
                     _ => false,
                 }
             };
+            ManualCard = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                ShouldCast = (mode, target, spellClass, damage) => !_castRed && !_castBlue && !_castYellow,
+            };
+            YellowCard = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                ShouldCast = (mode, target, spellClass, damage) => GetCard() switch
+                {
+                    Card.Gold => true,
+                    _ => false,
+                }
+            };
+            RedCard = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                ShouldCast = (mode, target, spellClass, damage) => GetCard() switch
+                {
+                    Card.Red => true,
+                    _ => false,
+                }
+            };
+            BlueCard = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                ShouldCast = (mode, target, spellClass, damage) => GetCard() switch
+                {
+                    Card.Blue => true,
+                    _ => false,
+                }
+            };
+        }
+
+        private bool _castRed;
+        private bool _castYellow;
+        private bool _castBlue;
+
+        private void KeyboardProvider_OnKeyPress(Keys keyBeingPressed, Oasys.Common.Tools.Devices.Keyboard.KeyPressState pressState)
+        {
+            if (!SpellW.SpellClass.IsSpellReady)
+            {
+                return;
+            }
+            if (keyBeingPressed == YellowCardKey && pressState == Oasys.Common.Tools.Devices.Keyboard.KeyPressState.Down)
+            {
+                if (ManualCard.ExecuteCastSpell())
+                {
+                    _castYellow = true;
+                }
+            }
+            if (keyBeingPressed == RedCardKey && pressState == Oasys.Common.Tools.Devices.Keyboard.KeyPressState.Down)
+            {
+                if (ManualCard.ExecuteCastSpell())
+                {
+                    _castRed = true;
+                }
+            }
+            if (keyBeingPressed == BlueCardKey && pressState == Oasys.Common.Tools.Devices.Keyboard.KeyPressState.Down)
+            {
+                if (ManualCard.ExecuteCastSpell())
+                {
+                    _castBlue = true;
+                }
+            }
+        }
+
+        internal override void OnCoreMainTick()
+        {
+            if (_castRed && RedCard.ExecuteCastSpell())
+            {
+                _castRed = false;
+            }
+            if (_castYellow && YellowCard.ExecuteCastSpell())
+            {
+                _castYellow = false;
+            }
+            if (_castBlue && BlueCard.ExecuteCastSpell())
+            {
+                _castBlue = false;
+            }
         }
 
         internal override void OnCoreMainInput()
@@ -61,6 +149,10 @@ namespace SixAIO.Champions
                 return;
             }
         }
+
+        public Keys YellowCardKey => WSettings.GetItem<KeyBinding>("Yellow Card").SelectedKey;
+        public Keys BlueCardKey => WSettings.GetItem<KeyBinding>("Blue Card").SelectedKey;
+        public Keys RedCardKey => WSettings.GetItem<KeyBinding>("Red Card").SelectedKey;
 
         internal override void InitializeMenu()
         {
@@ -72,6 +164,9 @@ namespace SixAIO.Champions
             QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
 
             WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+            WSettings.AddItem(new KeyBinding() { Title = "Yellow Card", SelectedKey = Keys.W });
+            WSettings.AddItem(new KeyBinding() { Title = "Blue Card", SelectedKey = Keys.E });
+            WSettings.AddItem(new KeyBinding() { Title = "Red Card", SelectedKey = Keys.T });
         }
     }
 }
