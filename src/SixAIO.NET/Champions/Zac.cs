b49dfb69191d96be4fcd08b404ab02b306f09e7d
SiX <dksovfen@gmail.com>
2023-01-05 16:34:09 +0100

    zac

---
+++ src/SixAIO.NET/Champions/Zac.cs
@@ -0,0 +1,113 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Extensions;
+using Oasys.Common.GameObject.Clients.ExtendedInstances.Spells;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.Common.Tools.Devices;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using Oasys.SDK.Tools;
+using SixAIO.Models;
+using System;
+using System.Linq;
+using static Oasys.Common.Logic.Orbwalker;
+
+namespace SixAIO.Champions
+{
+    internal sealed class Zac : Champion
+    {
+        public Zac()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                AllowCollision = (target, collisions) => !collisions.Any(),
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => QHitChance,
+                Range = () => 900,
+                Radius = () => 160,
+                Speed = () => 2800,
+                IsEnabled = () => UseQ,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault()
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                ShouldCast = (mode, target, spellClass, damage) => UnitManager.EnemyChampions.Any(x => TargetSelector.IsAttackable(x) && x.Distance <= 350 && x.IsAlive),
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                IsCharge = () => true,
+                CastPosition = (pos) =>
+                {
+                    Mouse.SetCursor(pos);
+                    return pos;
+                },
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => EHitChance,
+                Range = () => SpellE.ChargeTimer.IsRunning
+                                    ? SpellE.SpellClass.IsSpellReady
+                                            ? SpellE.ChargeTimer.ElapsedMilliseconds * 1.33f
+                                            : 0
+                                    : 1050 + SpellE.SpellClass.Level * 150,
+                Speed = () => 1500,
+                Radius = () => 250,
+                IsEnabled = () => UseE,
+                IsSpellReady = (spellClass, minMana, minCharges) => spellClass.IsSpellReady,
+                ShouldCast = (mode, target, spellClass, damage) =>
+                            target != null &&
+                            (SpellE.ChargeTimer.IsRunning
+                            ? target.Distance < SpellE.Range()
+                            : target.Distance < 1050 + SpellE.SpellClass.Level * 150),
+                TargetSelect = (mode) => SpellE.GetTargets(mode).FirstOrDefault()
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                IsEnabled = () => UseR,
+                ShouldCast = (mode, target, spellClass, damage) => UnitManager.EnemyChampions.Count(x => TargetSelector.IsAttackable(x) && x.Distance < REnemiesCloserThan) > RIfMoreThanEnemiesNear,
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            SpellE.ExecuteCastSpell();
+            SpellQ.ExecuteCastSpell();
+            SpellW.ExecuteCastSpell();
+            SpellR.ExecuteCastSpell();
+        }
+
+        private int RIfMoreThanEnemiesNear
+        {
+            get => RSettings.GetItem<Counter>("R If More Than Enemies Near").Value;
+            set => RSettings.GetItem<Counter>("R If More Than Enemies Near").Value = value;
+        }
+
+        private int REnemiesCloserThan
+        {
+            get => RSettings.GetItem<Counter>("R Enemies Closer Than").Value;
+            set => RSettings.GetItem<Counter>("R Enemies Closer Than").Value = value;
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Zac)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+            MenuTab.AddGroup(new Group("R Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            ESettings.AddItem(new ModeDisplay() { Title = "E HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+
+            RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            RSettings.AddItem(new Counter() { Title = "R If More Than Enemies Near", MinValue = 0, MaxValue = 5, Value = 1, ValueFrequency = 1 });
+            RSettings.AddItem(new Counter() { Title = "R Enemies Closer Than", MinValue = 50, MaxValue = 500, Value = 300, ValueFrequency = 50 });
+        }
+    }
+}
