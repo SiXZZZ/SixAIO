a67e00333abdde153854e7b8ee32207d9ea7fb5d
SiX <dksovfen@gmail.com>
2023-01-15 17:02:27 +0100

    jinx

--- src/SixAIO.NET/Champions/Jinx.cs
+++ src/SixAIO.NET/Champions/Jinx.cs
@@ -5,12 +5,9 @@ using Oasys.Common.Menu.ItemComponents;
 using Oasys.SDK;
 using Oasys.SDK.Menu;
 using Oasys.SDK.SpellCasting;
-using Oasys.SDK.Tools;
-using SixAIO.Helpers;
 using SixAIO.Models;
 using System;
 using System.Linq;
-using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace SixAIO.Champions
