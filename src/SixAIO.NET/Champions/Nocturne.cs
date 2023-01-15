e4c09abb0cf251ecdc4a37bbda63b480693ab755
SiX <dksovfen@gmail.com>
2023-01-15 15:44:08 +0100

    Nocturne

---
+++ src/SixAIO.NET/Champions/Nocturne.cs
@@ -0,0 +1,55 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Menu;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System;
+using System.Linq;
+
+namespace SixAIO.Champions
+{
+    internal sealed class Nocturne : Champion
+    {
+        public Nocturne()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => QHitChance,
+                Range = () => 1150,
+                Radius = () => 120,
+                Speed = () => 1600,
+                IsEnabled = () => UseQ,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault()
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                IsTargetted = () => true,
+                Range = () => 425,
+                IsEnabled = () => UseE,
+                TargetSelect = (mode) => SpellE.GetTargets(mode).FirstOrDefault()
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            SpellQ.ExecuteCastSpell();
+            SpellE.ExecuteCastSpell();
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Nocturne)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+
+        }
+    }
+}
