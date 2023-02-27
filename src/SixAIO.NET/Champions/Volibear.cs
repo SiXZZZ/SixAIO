993917a2880e4acc0217a9c3f8e2016f50618c7f
SiX <dksovfen@gmail.com>
2023-02-27 20:22:44 +0100

    voli

---
+++ src/SixAIO.NET/Champions/Volibear.cs
@@ -0,0 +1,89 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System;
+using System.Linq;
+using Oasys.Common.Menu;
+using Oasys.SDK;
+
+namespace SixAIO.Champions
+{
+    internal sealed class Volibear : Champion
+    {
+        public Volibear()
+        {
+            Orbwalker.OnOrbwalkerAfterBasicAttack += Orbwalker_OnOrbwalkerAfterBasicAttack;
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                IsEnabled = () => UseQ,
+                ShouldCast = (mode, target, spellClass, damage) => TargetSelector.IsAttackable(Orbwalker.TargetHero) && TargetSelector.IsInRange(Orbwalker.TargetHero),
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsTargetted = () => true,
+                Range = () => 325,
+                IsEnabled = () => UseW,
+                TargetSelect = (mode) => SpellW.GetTargets(mode).FirstOrDefault(),
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => EHitChance,
+                Delay = () => 2f,
+                Range = () => 1200,
+                Radius = () => 325,
+                Speed = () => float.MaxValue,
+                IsEnabled = () => UseE,
+                TargetSelect = (mode) => SpellE.GetTargets(mode).FirstOrDefault()
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => RHitChance,
+                Delay = () => 0f,
+                Range = () => 700,
+                Speed = () => 750,
+                Radius = () => 300,
+                IsEnabled = () => UseR,
+                TargetSelect = (mode) => SpellR.GetTargets(mode).FirstOrDefault()
+            };
+        }
+
+        private void Orbwalker_OnOrbwalkerAfterBasicAttack(float gameTime, Oasys.Common.GameObject.GameObjectBase target)
+        {
+            if (target is not null && target.IsAlive && target.IsObject(ObjectTypeFlag.AIHeroClient))
+            {
+                SpellQ.ExecuteCastSpell();
+            }
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            SpellW.ExecuteCastSpell();
+            SpellE.ExecuteCastSpell();
+            SpellR.ExecuteCastSpell();
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Volibear)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+            MenuTab.AddGroup(new Group("R Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            ESettings.AddItem(new ModeDisplay() { Title = "E HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+            RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            RSettings.AddItem(new ModeDisplay() { Title = "R HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
+
+        }
+    }
+}
