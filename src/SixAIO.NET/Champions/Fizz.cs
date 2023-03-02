32c57ab6be9ab90c1cea49927b4f4f9770a0c631
SiX <dksovfen@gmail.com>
2023-03-02 18:03:58 +0100

    fizz

---
+++ src/SixAIO.NET/Champions/Fizz.cs
@@ -0,0 +1,124 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System;
+using System.Linq;
+using Oasys.Common.Menu;
+using Oasys.SDK;
+using Oasys.Common.GameObject;
+
+namespace SixAIO.Champions
+{
+    internal sealed class Fizz : Champion
+    {
+        internal Spell SpellR2;
+        internal Spell SpellR3;
+
+        public Fizz()
+        {
+            Spell.OnSpellCast += Spell_OnSpellCast;
+            Orbwalker.OnOrbwalkerAfterBasicAttack += Orbwalker_OnOrbwalkerAfterBasicAttack;
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                IsTargetted = () => true,
+                Range = () => 550,
+                IsEnabled = () => UseQ,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault()
+            };
+            SpellW = new Spell(CastSlot.W, SpellSlot.W)
+            {
+                IsEnabled = () => UseW,
+                ShouldCast = (mode, target, spellClass, damage) => TargetSelector.IsAttackable(Orbwalker.TargetHero) && TargetSelector.IsInRange(Orbwalker.TargetHero),
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                AllowCollision = (target, collisions) => !collisions.Any(),
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Circle,
+                MinimumHitChance = () => EHitChance,
+                Delay = () => 0.25f,
+                Range = () => 350,
+                Radius = () => 200,
+                Speed = () => 1200,
+                IsEnabled = () => UseE && SpellE.SpellClass.SpellData.SpellName == "FizzE",
+                TargetSelect = (mode) => SpellE.GetTargets(mode).FirstOrDefault()
+            };
+            SpellR = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => RHitChance,
+                Range = () => 1300,
+                Speed = () => 1300,
+                Radius = () => 150,
+                IsEnabled = () => UseR,
+                TargetSelect = (mode) => SpellR.GetTargets(mode).FirstOrDefault()
+            };
+            SpellR2 = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => RHitChance,
+                Range = () => 1300,
+                Speed = () => 1300,
+                Radius = () => 250,
+                IsEnabled = () => UseR,
+                TargetSelect = (mode) => SpellR2.GetTargets(mode).FirstOrDefault()
+            };
+            SpellR3 = new Spell(CastSlot.R, SpellSlot.R)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => RHitChance,
+                Range = () => 1300,
+                Speed = () => 1300,
+                Radius = () => 350,
+                IsEnabled = () => UseR,
+                TargetSelect = (mode) => SpellR3.GetTargets(mode).FirstOrDefault()
+            };
+        }
+
+        private void Spell_OnSpellCast(SDKSpell spell, GameObjectBase target)
+        {
+            if (spell is not null && spell.SpellSlot == SpellSlot.Q)
+            {
+                SpellW.ExecuteCastSpell();
+            }
+        }
+
+        private void Orbwalker_OnOrbwalkerAfterBasicAttack(float gameTime, GameObjectBase target)
+        {
+            if (target is not null && target.IsObject(ObjectTypeFlag.AIHeroClient))
+            {
+                SpellW.ExecuteCastSpell();
+            }
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            SpellR.ExecuteCastSpell();
+            SpellR2.ExecuteCastSpell();
+            SpellR3.ExecuteCastSpell();
+            SpellQ.ExecuteCastSpell();
+            SpellE.ExecuteCastSpell();
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Fizz)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("W Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+            MenuTab.AddGroup(new Group("R Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+
+            WSettings.AddItem(new Switch() { Title = "Use W", IsOn = true });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            ESettings.AddItem(new ModeDisplay() { Title = "E HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "VeryHigh" });
+
+            RSettings.AddItem(new Switch() { Title = "Use R", IsOn = true });
+            RSettings.AddItem(new ModeDisplay() { Title = "R HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "VeryHigh" });
+
+        }
+    }
+}
