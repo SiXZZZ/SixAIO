bb88b7aa46087aadd9bde536ae56d7e8a6168a25
SiX <dksovfen@gmail.com>
2023-03-12 16:29:07 +0100

    kled qe

---
+++ src/SixAIO.NET/Champions/Kled.cs
@@ -0,0 +1,63 @@
+﻿using Oasys.Common.Enums.GameEnums;
+using Oasys.Common.Menu.ItemComponents;
+using Oasys.SDK.Menu;
+using Oasys.SDK.SpellCasting;
+using SixAIO.Models;
+using System;
+using System.Linq;
+using Oasys.Common.Menu;
+using Oasys.SDK;
+
+namespace SixAIO.Champions
+{
+    internal sealed class Kled : Champion
+    {
+        public Kled()
+        {
+            SpellQ = new Spell(CastSlot.Q, SpellSlot.Q)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => QHitChance,
+                Range = () => 800,
+                Speed = () => 1500,
+                Radius = () => 100,
+                IsEnabled = () => UseQ,
+                IsSpellReady = (spellClass, minimumMana, minimumCharges) => spellClass.IsSpellReady,
+                TargetSelect = (mode) => SpellQ.GetTargets(mode).FirstOrDefault()
+            };
+            SpellE = new Spell(CastSlot.E, SpellSlot.E)
+            {
+                PredictionMode = () => Prediction.MenuSelected.PredictionType.Line,
+                MinimumHitChance = () => EHitChance,
+                Delay = () => 0f,
+                Range = () => 550,
+                Speed = () => 600 + UnitManager.MyChampion.UnitStats.MoveSpeed,
+                Radius = () => 120,
+                IsEnabled = () => UseE && SpellQ.SpellClass.SpellData.SpellName == "KledQ",
+                IsSpellReady = (spellClass, minimumMana, minimumCharges) => spellClass.IsSpellReady,
+                TargetSelect = (mode) => SpellE.GetTargets(mode).FirstOrDefault()
+            };
+        }
+
+        internal override void OnCoreMainInput()
+        {
+            SpellQ.ExecuteCastSpell();
+            SpellE.ExecuteCastSpell();
+        }
+
+        internal override void InitializeMenu()
+        {
+            MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Kled)}"));
+            MenuTab.AddGroup(new Group("Q Settings"));
+            MenuTab.AddGroup(new Group("E Settings"));
+
+            QSettings.AddItem(new Switch() { Title = "Use Q", IsOn = true });
+            QSettings.AddItem(new ModeDisplay() { Title = "Q HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "VeryHigh" });
+
+            ESettings.AddItem(new Switch() { Title = "Use E", IsOn = true });
+            ESettings.AddItem(new ModeDisplay() { Title = "E HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "VeryHigh" });
+
+
+        }
+    }
+}
