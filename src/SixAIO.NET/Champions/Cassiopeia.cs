2055f418efed5250d92a4ba97f27163488aa272d
0xSquanchy <140261299+0xSquanchy@users.noreply.github.com>
2023-07-23 16:26:37 +0200

    Update Cassiopeia.cs
    Added an option to set the minimum amount of enemies that should be facing Cassiopeia when casting her ultimate

--- src/SixAIO.NET/Champions/Cassiopeia.cs
+++ src/SixAIO.NET/Champions/Cassiopeia.cs
@@ -57,10 +57,8 @@ namespace SixAIO.Champions
                 IsEnabled = () => UseE,
                 TargetSelect = (mode) =>
                 {
-                    if(UseELasthit && mode == Orbwalker.OrbWalkingModeType.LastHit)
-                    {
+                    if (UseELasthit && mode == Orbwalker.OrbWalkingModeType.LastHit)
                         return SpellE.GetTargets(mode, x => x.PredictHealth(150) <= GetEDamage(x, SpellE.SpellClass)).OrderBy(IsPoisoned).ThenBy(x => x.EffectiveMagicHealth).FirstOrDefault();
-                    }
 
                     var target = SpellE.GetTargets(mode).OrderBy(IsPoisoned).ThenBy(x => x.EffectiveMagicHealth).FirstOrDefault();
                     if (UseELasthit)
@@ -96,10 +94,13 @@ namespace SixAIO.Champions
                 Radius = () => 80,
                 Delay = () => 0.5f,
                 IsEnabled = () => UseR,
-                TargetSelect = (mode) => SpellR.GetTargets(mode, x =>
+                TargetSelect = (mode) => {
+                    var targets = SpellRSemiAuto.GetTargets(mode, x =>
                                                       x.IsFacing(UnitManager.MyChampion) &&
-                                                      !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Magical, false))
-                                                .FirstOrDefault()
+                                                      !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Magical, false));
+
+                    return (targets != null && targets.Count() >= RMinimumEnemiesCount) ? targets.FirstOrDefault() : null;
+                }
             };
             SpellRSemiAuto = new Spell(CastSlot.R, SpellSlot.R)
             {
@@ -110,10 +111,13 @@ namespace SixAIO.Champions
                 Radius = () => 80,
                 Delay = () => 0.5f,
                 IsEnabled = () => UseSemiAutoR,
-                TargetSelect = (mode) => SpellRSemiAuto.GetTargets(mode, x =>
+                TargetSelect = (mode) => {
+                    var targets = SpellRSemiAuto.GetTargets(mode, x =>
                                                       x.IsFacing(UnitManager.MyChampion) &&
-                                                      !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Magical, false))
-                                                .FirstOrDefault()
+                                                      !TargetSelector.IsInvulnerable(x, Oasys.Common.Logic.DamageType.Magical, false));
+
+                    return (targets != null && targets.Count() >= RMinimumEnemiesCount) ? targets.FirstOrDefault() : null;
+                }
             };
         }
 
@@ -211,6 +215,12 @@ namespace SixAIO.Champions
 
         public Keys DisableAAKey => MenuTab.GetItem<KeyBinding>("Disable AA Key").SelectedKey;
 
+        private int RMinimumEnemiesCount
+        {
+            get => RSettings.GetItem<Counter>("Minimum enemies facing for R").Value;
+            set => RSettings.GetItem<Counter>("Minimum enemies facing for R").Value = value;
+        }
+
         internal override void InitializeMenu()
         {
             MenuManager.AddTab(new Tab($"SIXAIO - {nameof(Cassiopeia)}"));
@@ -239,7 +249,7 @@ namespace SixAIO.Champions
             RSettings.AddItem(new Switch() { Title = "Use Semi Auto R", IsOn = true });
             RSettings.AddItem(new KeyBinding() { Title = "Semi Auto R Key", SelectedKey = Keys.T });
             RSettings.AddItem(new ModeDisplay() { Title = "Semi Auto R HitChance", ModeNames = Enum.GetNames(typeof(Prediction.MenuSelected.HitChance)).ToList(), SelectedModeName = "High" });
-
+            RSettings.AddItem(new Counter() { Title = "Minimum enemies facing for R", MinValue = 1, MaxValue = 5, Value = 1 });
 
             MenuTab.AddDrawOptions(SpellSlot.Q, SpellSlot.W, SpellSlot.E, SpellSlot.R);
 
